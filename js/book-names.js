8408b810f95cf413d62be6267290bdb175a6ba61
nrp0450 <66805737+nrp0450@users.noreply.github.com>
2020-06-15 18:47:42 -0500

    fixed space after dash

--- js/book-names.js
+++ js/book-names.js
@@ -1,4 +1,4 @@
 var books = [
-  { name: "Luke by chapter", ref: "42-LUK-001.html" },
-  { name: "Luke by Set", ref: "00-LUK-2-001.html" },
+  { name: "Luke (by ch)", ref: "42-LUK-001.html" },
+  { name: "Luke (by set)", ref: "00-LUK-2-001.html" },
 ];
