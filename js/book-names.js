d0e29baf2bdc3263963221b001dc1d81dfe52b91
nrp0450 <66805737+nrp0450@users.noreply.github.com>
2020-06-15 18:34:47 -0500

    added all audio

--- js/book-names.js
+++ js/book-names.js
@@ -1,4 +1,4 @@
 var books = [
-  { name: "Luk by chapter", ref: "42-LUK-001.html" },
-  { name: "LUKE by Set", ref: "00-LUK-2-001.html" },
+  { name: "Luke by chapter", ref: "42-LUK-001.html" },
+  { name: "Luke by Set", ref: "00-LUK-2-001.html" },
 ];
