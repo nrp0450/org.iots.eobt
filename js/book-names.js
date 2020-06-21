e4ee328549631bb77ac7dbecdfe63ea5bee8c18c
nrp0450 <66805737+nrp0450@users.noreply.github.com>
2020-06-21 17:33:06 -0500

    added and removed books to make the sets book open as default

--- js/book-names.js
+++ js/book-names.js
@@ -1,4 +1,4 @@
 var books = [
-  { name: "Luke by set", ref: "00-LUK-2-001.html" },
-  { name: "Luke by ch", ref: "42-LUK-001.html" },
+  { name: "Luke by set", ref: "00-LUKSETS-001.html" },
+  { name: "Luke by ch", ref: "00-LUKCHAP-2-001.html" },
 ];
