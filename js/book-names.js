7c32e1b23226262d7d706cb298c8e65244b1503c
nrp0450 <66805737+nrp0450@users.noreply.github.com>
2020-06-29 16:10:39 -0500

    re-added books to get it to start on "by set"

--- js/book-names.js
+++ js/book-names.js
@@ -1,4 +1,4 @@
 var books = [
   { name: "Luke by set", ref: "00-LUKSETS-001.html" },
-  { name: "Luke by ch", ref: "00-LUKCHAP-2-001.html" },
+  { name: "Luke by Ch", ref: "00-LUKCHAP-001.html" },
 ];
